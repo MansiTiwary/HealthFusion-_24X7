ebb0b8fc38ac15f7928805ce49a578cd91df015d
MansiTiwary <mansikumari117@gmail.com>
2025-11-13 16:30:49 +0530

    Milestone-0 HMS-MAD-1

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from date import datetime
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///healthfusion.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
---
+++ model.py
@@ -0,0 +1,21 @@
+from app import db 
+
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+    contact = db.Column(db.String(30), nullable=True)
+    specialization_id = db.Column(db.Integer, db.ForeignKey('departments.id'), nullable=True)
+    is_active = db.Column(db.Boolean, default=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+class department(db.Model):
+    __tablename__ = 'departments'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.Text, nullable=True)
